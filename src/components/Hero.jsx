d0f5759fdd2831d6f2021008521ad79f01fa9737
ibrahimalqadhi711-tech <ibrahimalqadhi711@gmail.com>
2026-04-27 22:09:27 +0300

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,6 +8,7 @@ const Hero = () => {
       <div className="hero-content">
         <p className="hero-greeting">Hi, my name is</p>
         <h1 className="hero-name">Ibrahim.</h1>
+      <img src={myImage} alt="This is my picture" className='profile-img' />
         <h2 className="hero-title">I'm a Frontend Developer.</h2>
         <p className="hero-description">
           I specialize in building (and occasionally designing) exceptional, high-quality digital experiences. I focus on creating highly responsive, accessible, and user-friendly web applications.
@@ -17,7 +18,6 @@ const Hero = () => {
           <a href="#contact" className="button button-outline">Contact Me</a>
         </div>
       </div>
-      <img src={myImage} alt="This is my picture" className='profile-img' />
     </section>
   );
 };
